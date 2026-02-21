515cb219fa391b77d4055707896a168b441d7923
Jakob@WSL <Jakob@WSL>
2026-02-21 23:11:36 +0100

    Update server port configuration from 3000 to 3002 + Dockerfile

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const ContentIndexer = require('./indexer');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(express.json());
